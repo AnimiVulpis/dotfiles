095d2e9608dcb241669b0a09a71bcd333ef05c78
AnimiVulpis <animi.vulpis@gmail.com>
2020-06-16 09:09:33 +0200

    Work on arch based zsh config

---
+++ arch-zsh/completion.zsh
@@ -0,0 +1,21 @@
+# modify zsh completion
+# 0 -- smart case like completion (plus hypen-insensitive)
+# 1 -- *add* (the `+`) full case insensitivity
+# 2 -- word completion from right side (and on specific anchors)
+# 3 -- word completion from left side
+zstyle ':completion:*' matcher-list \
+	'm:{[:lower:]-_}={[:upper:]_-}' \
+	'+m:{[:upper:]}={[:lower:]}' \
+	'r:|[-_./]=* r:|=*' \
+	'l:|=* r:|=*'
+
+# use default LS_COLORS for list entries
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+
+# use menu selection for completion lists
+zstyle ':completion:*' menu select
+
+# load complist module to make the `menuselect` keymap available
+zmodload zsh/complist
+# bind <shift-tab> to go back through menu selection list
+bindkey -M menuselect '^\e[Z' reverse-menu-complete
