ce8975895935f5afc7b9872abe1edbaf90dac8e7
AnimiVulpis <animi.vulpis@gmail.com>
2020-08-29 18:28:22 +0200

    Change repository update script to be more compact

--- update-arch.sh
+++ update-arch.sh
@@ -2,6 +2,6 @@
 sudo pacman -Syu
 sudo snap refresh
 flatpak update
-~/dotfiles/repository-updates.sh
+~/dotfiles/repository-updates.sh ~/aur-and-external/
 sudo fwupdmgr refresh &&
   sudo fwupdmgr get-updates
