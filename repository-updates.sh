aea8ff93b50aa6ac43724fe5866191cf6c14cea0
AnimiVulpis <animi.vulpis@gmail.com>
2020-08-29 18:28:22 +0200

    Change repository update script to be more compact

--- repository-updates.sh
+++ repository-updates.sh
@@ -1,5 +1,20 @@
 #!/bin/zsh
-exa -D ~/aur-and-external |
-  xargs -n1 -I@ zsh -c \
-    "git -C ~/aur-and-external/@ fetch -v origin 2>&1 |
-      rg --passthru -e '^   [\[0-9a-f\.]+'"
+
+# Check if first arugment is a valid directory
+if [ ! -d "$1" ]; then
+  echo "$1 is not valid directory"
+  exit 0
+fi
+
+# Fetch updates and print errors
+exa -D $1 |
+  xargs -n1 -I@ zsh -c "
+  echo -ne '\033[2K Fetching @\r';
+  git -C $1/@ fetch -q origin || echo -e ' Fetching @ \033[0;31mfailed\033[0m (See above for error)'"
+
+# Pretty-print tracking information
+exa -D $1 |
+  xargs -n1 -I@ zsh -c "
+  echo -n '@\t';
+  git -c color.status=always -C $1/@ status --untracked-files=no --short --branch" 2>&1 |
+  column --table --separator '	'
