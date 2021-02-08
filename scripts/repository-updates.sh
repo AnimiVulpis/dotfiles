f3a97b685784ac4f5fdcf29e7f3b6c7c74aa22b3
AnimiVulpis <animi.vulpis@gmail.com>
2021-02-08 09:56:24 +0100

    Increase indentation to 4 spaces

--- scripts/repository-updates.sh
+++ scripts/repository-updates.sh
@@ -2,19 +2,19 @@
 
 # Check if first argument is a valid directory
 if [ ! -d "$1" ]; then
-  echo "$1 is not valid directory"
-  exit 0
+    echo "$1 is not valid directory"
+    exit 0
 fi
 
 # Fetch updates and print errors
 exa -D $1 |
-  xargs -I@ zsh -c "
-  echo -ne '\033[2K Fetching @\r';
-  git -C $1/@ fetch -q origin || echo -e ' Fetching @ \033[0;31mfailed\033[0m (See above for error)'"
+    xargs -I@ zsh -c "
+    echo -ne '\033[2K Fetching @\r';
+    git -C $1/@ fetch -q origin || echo -e ' Fetching @ \033[0;31mfailed\033[0m (See above for error)'"
 
 # Pretty-print tracking information
 exa -D $1 |
-  xargs -I@ zsh -c "
-  echo -n '@\t';
-  git -c color.status=always -C $1/@ status --untracked-files=no --short --branch" 2>&1 |
-  column --table --separator '	'
+    xargs -I@ zsh -c "
+    echo -n '@\t';
+    git -c color.status=always -C $1/@ status --untracked-files=no --short --branch" 2>&1 |
+    column --table --separator '	'
