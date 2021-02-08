c8fe18cc60e1cf737ab6da85bac602e21e77a810
AnimiVulpis <animi.vulpis@gmail.com>
2021-02-08 09:59:22 +0100

    Fix some misplaced file redirect

--- scripts/pacman-mirrorlist-update.sh
+++ scripts/pacman-mirrorlist-update.sh
@@ -4,7 +4,7 @@ set -e
 
 mirrorlist_url="https://archlinux.org/mirrorlist/?country=HR&country=CZ&country=DK&country=FI&country=FR&country=DE&country=IE&country=IT&country=LV&country=LT&country=LU&country=NL&country=NO&country=PL&country=SE&protocol=https&ip_version=4&use_mirror_status=on"
 
-curl -s "$mirrorlist_url" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 30 - ~/downloads/mirrorlist >!
+curl -s "$mirrorlist_url" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 30 - >! ~/downloads/mirrorlist
 
 sudo mv -vi ~/downloads/mirrorlist /etc/pacman.d/mirrorlist
 
