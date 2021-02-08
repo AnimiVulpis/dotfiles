55331e3dac26aa1843187938b6cc84834e39f033
AnimiVulpis <animi.vulpis@gmail.com>
2021-02-08 22:53:04 +0100

    Fix 3 monitor xrandr setup call

--- scripts/display-setup.sh
+++ scripts/display-setup.sh
@@ -9,7 +9,7 @@ DP2_2=$(xrandr | grep -o "DP-2-2 connected" || echo '')
 DP2_3=$(xrandr | grep -o "DP-2-3 connected" || echo '')
 
 if [ -n "$DP2_2" ] && [ -n "$DP2_3" ]; then
-    xrandr --output DP-2-2 --auto --output eDP-1 --auto --below DP-2-2 --primary --output DP-2-3 --auto --pos 1920x-180
+    xrandr --output DP-2-2 --pos 0x0 --auto --output eDP-1 --auto --below DP-2-2 --primary --output DP-2-3 --auto --rotate left --pos 1920x-180
 else
     if [ -n "$DP2_2" ]; then
         xrandr --output DP-2-2 --auto --output eDP-1 --auto --below DP-2-2 --primary
