2f70e4662da8b6aea81170fb4aaf3691b2877726
AnimiVulpis <animi.vulpis@gmail.com>
2021-02-08 09:40:06 +0100

    Fix a glitch in the update script
    `xargs: warning: options --max-args and --replace/-I/-i are mutually exclusive, ignoring previous --max-args value`
    Also: `-I` implies `-L ` (which itself is similar to `-n`

--- scripts/repository-updates.sh
+++ scripts/repository-updates.sh
@@ -8,13 +8,13 @@ fi
 
 # Fetch updates and print errors
 exa -D $1 |
-  xargs -n1 -I@ zsh -c "
+  xargs -I@ zsh -c "
   echo -ne '\033[2K Fetching @\r';
   git -C $1/@ fetch -q origin || echo -e ' Fetching @ \033[0;31mfailed\033[0m (See above for error)'"
 
 # Pretty-print tracking information
 exa -D $1 |
-  xargs -n1 -I@ zsh -c "
+  xargs -I@ zsh -c "
   echo -n '@\t';
   git -c color.status=always -C $1/@ status --untracked-files=no --short --branch" 2>&1 |
   column --table --separator '	'
