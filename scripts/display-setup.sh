f2336d2cb331bd05d8dfbb17046f18898c71c452
AnimiVulpis <animi.vulpis@gmail.com>
2021-02-08 15:15:20 +0100

    Add 3 monitor setup

--- scripts/display-setup.sh
+++ scripts/display-setup.sh
@@ -5,10 +5,20 @@ if [ -z "$(which xrandr)" ]; then
     exit 1
 fi
 
+DP2_2=$(xrandr | grep -o "DP-2-2 connected" || echo '')
 DP2_3=$(xrandr | grep -o "DP-2-3 connected" || echo '')
 
-if [ -n "$DP2_3" ]; then
-    xrandr --output DP-2-3 --auto --output eDP-1 --auto --below DP-2-3 --primary
+if [ -n "$DP2_2" ] && [ -n "$DP2_3" ]; then
+    xrandr --output DP-2-2 --auto --output eDP-1 --auto --below DP-2-2 --primary --output DP-2-3 --auto --pos 1920x-180
 else
-    xrandr --output DP-2-3 --off --output eDP-1 --auto --primary
+    if [ -n "$DP2_2" ]; then
+        xrandr --output DP-2-2 --auto --output eDP-1 --auto --below DP-2-2 --primary
+    else
+        xrandr --output DP-2-2 --off --output eDP-1 --auto --primary
+    fi
+    if [ -n "$DP2_3" ]; then
+        xrandr --output DP-2-3 --auto --output eDP-1 --auto --below DP-2-3 --primary
+    else
+        xrandr --output DP-2-3 --off --output eDP-1 --auto --primary
+    fi
 fi
