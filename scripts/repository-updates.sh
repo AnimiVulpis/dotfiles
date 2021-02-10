856f68288eb24e76ab0c6450db96764e683fcb5c
AnimiVulpis <animi.vulpis@gmail.com>
2021-02-10 13:25:37 +0100

    Auto reformat repository update script

--- scripts/repository-updates.sh
+++ scripts/repository-updates.sh
@@ -10,17 +10,17 @@ fi
 echo -ne '\e[?25l'
 
 # Fetch updates and print errors
-exa -D $1 |
-    xargs -I@ zsh -c "
+exa -D $1 \
+    | xargs -I@ zsh -c "
     echo -ne '\e[2KFetching @\r';
     git -C $1/@ fetch -q origin || echo -e 'Fetching @ \e[0;31mfailed\e[0m (See above for error)'"
 
 # Pretty-print tracking information
-exa -D $1 |
-    xargs -I@ zsh -c "
+exa -D $1 \
+    | xargs -I@ zsh -c "
     echo -n '@\t';
-    git -c color.status=always -C $1/@ status --untracked-files=no --short --branch" 2>&1 |
-    column --table --separator '	'
+    git -c color.status=always -C $1/@ status --untracked-files=no --short --branch" 2>&1 \
+    | column --table --separator '	'
 
 # Show cursor again
 echo -ne '\e[?25h'
