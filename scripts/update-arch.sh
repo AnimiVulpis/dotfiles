b2bb603d653074a62f50f43a68e0226fbf05ab63
AnimiVulpis <animi.vulpis@gmail.com>
2021-02-10 13:23:16 +0100

    Add cabal install call to update script and reformat

--- scripts/update-arch.sh
+++ scripts/update-arch.sh
@@ -1,7 +1,8 @@
 #!/bin/zsh
 sudo pacman --color always -Syu
+cabal install --overwrite-policy=always arbtt
 sudo snap refresh
 flatpak update
 ~/dotfiles/scripts/repository-updates.sh ~/aur-and-external/
-sudo fwupdmgr refresh &&
-  sudo fwupdmgr get-updates
+sudo fwupdmgr refresh \
+    && sudo fwupdmgr get-updates
