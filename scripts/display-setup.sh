244fbfe761a5d8f44bb60ad93f28345ec698a268
AnimiVulpis <animi.vulpis@gmail.com>
2021-02-16 09:05:21 +0100

    Change display script for new monitor setup

--- scripts/display-setup.sh
+++ scripts/display-setup.sh
@@ -9,16 +9,13 @@ DP2_2=$(xrandr | grep -o "DP-2-2 connected" || echo '')
 DP2_3=$(xrandr | grep -o "DP-2-3 connected" || echo '')
 
 if [ -n "$DP2_2" ] && [ -n "$DP2_3" ]; then
-    xrandr --output DP-2-2 --pos 0x0 --auto --output eDP-1 --auto --below DP-2-2 --primary --output DP-2-3 --auto --rotate left --pos 1920x-180
+    xrandr --output DP-2-2 --pos 0x0 --auto --output DP-2-3 --auto --right-of DP-2-2 --output eDP-1 --auto --primary --pos 960x1200
 else
     if [ -n "$DP2_2" ]; then
-        xrandr --output DP-2-2 --auto --output eDP-1 --auto --below DP-2-2 --primary
+        xrandr --output DP-2-2 --auto --output eDP-1 --auto --below DP-2-2 --primary --output DP-2-3 --off
+    elif [ -n "$DP2_3" ]; then
+        xrandr --output DP-2-2 --off --output DP-2-3 --auto --output eDP-1 --auto --below DP-2-3 --primary
     else
-        xrandr --output DP-2-2 --off --output eDP-1 --auto --primary
-    fi
-    if [ -n "$DP2_3" ]; then
-        xrandr --output DP-2-3 --auto --output eDP-1 --auto --below DP-2-3 --primary
-    else
-        xrandr --output DP-2-3 --off --output eDP-1 --auto --primary
+        xrandr --output DP-2-2 --off --output DP-2-3 --off --output eDP-1 --auto --primary
     fi
 fi
