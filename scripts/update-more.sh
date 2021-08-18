03cbf045590d4fd89024d18f0f82f80527141c59
AnimiVulpis <animi.vulpis@gmail.com>
2021-08-18 08:25:15 +0200

    Modify go install script part
    `go get -v -u` was deprecated. Using `go install pkg@version` instead

--- scripts/update-more.sh
+++ scripts/update-more.sh
@@ -9,14 +9,14 @@ cargo install chars click eva genact git-delta
 
 # Go related
 # Tool to fetch gitignore files from GitHub templates `gig`
-echo '\e[34mExecuting\e[0m "\e[32mgo \e[37mget \e[36m-v -u \e[37mgithub.com/toshi0607/gig\e[0m"'
-go get -v -u github.com/toshi0607/gig
+echo '\e[34mExecuting\e[0m "\e[32mgo \e[37minstall github.com/toshi0607/gig@latest\e[0m"'
+go install github.com/toshi0607/gig@latest
 # Tool to check the size of git repositories `git-sizer`
-echo '\e[34mExecuting\e[0m "\e[32mgo \e[37mget \e[36m-v -u \e[37mgithub.com/github/git-sizer\e[0m"'
-go get -v -u github.com/github/git-sizer
+echo '\e[34mExecuting\e[0m "\e[32mgo \e[37minstall github.com/github/git-sizer@latest\e[0m"'
+go install github.com/github/git-sizer@latest
 # Tool to manage hosts file idempotent `hostess`
-echo '\e[34mExecuting\e[0m "\e[32mgo \e[37mget \e[36m-v -u \e[37mgithub.com/cbednarski/hostess\e[0m"'
-go get -v -u github.com/cbednarski/hostess
+echo '\e[34mExecuting\e[0m "\e[32mgo \e[37minstall github.com/cbednarski/hostess@latest\e[0m"'
+go install github.com/cbednarski/hostess@latest
 # Tool for fast code search `houndd`
-echo '\e[34mExecuting\e[0m "\e[32mgo \e[37mget \e[36m-v -u \e[37mgithub.com/hound-search/hound/cmds/...\e[0m"'
-go get -v -u github.com/hound-search/hound/cmds/...
+echo '\e[34mExecuting\e[0m "\e[32mgo \e[37minstall github.com/hound-search/hound/cmds/...@latest\e[0m"'
+go install github.com/hound-search/hound/cmds/...@latest
