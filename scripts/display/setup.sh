7f1d742be76a179095ee4fdf72f5108ca1d3bf01
AnimiVulpis <animi.vulpis@gmail.com>
2021-08-31 14:08:36 +0200

    Add resetting display if not connected

--- scripts/display/setup.sh
+++ scripts/display/setup.sh
@@ -17,4 +17,6 @@ DP2_1=$(xrandr | grep -o "DP-2-1 connected" || echo '')
 
 if [ -n "${DP2_1}" ]; then
     xrandr --output DP-2-1 --pos 0x0 --auto --output eDP-1 --auto --primary --pos 760x1440
+else
+    xrandr --output DP-2-1 --off --output eDP-1 --auto --primary
 fi
