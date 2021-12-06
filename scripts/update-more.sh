8a626e56482939fe7fb39f8c2abace24e9d168d9
AnimiVulpis <animi.vulpis@gmail.com>
2021-12-06 15:41:23 +0100

    Update scripts folder

--- scripts/update-more.sh
+++ scripts/update-more.sh
@@ -1,20 +1,37 @@
-#!/bin/zsh
+#!/bin/sh
 
 # Rust related
 echo '\e[34mExecuting\e[0m "\e[32mrustup \e[37mupdate\e[0m"'
 rustup update
 
+# List all installed rust crates
+# cargo install --list
+
 echo '\e[34mExecuting\e[0m "\e[32mcargo \e[37minstall chars click eva genact git-delta gobang tidy-viewer\e[0m"'
 cargo install \
-    chars \
+    bat \
+    broot \
     click \
     eva \
+    exa \
+    fd-find \
+    find_unicode \
     genact \
     git-delta \
+    ncspot \
+    swaysome \
+    sworkstyle \
     tidy-viewer
 cargo install --version 0.1.0-alpha.5 gobang
 
 # Go related
+
+# List all installed go packages
+# go list ...
+
+# Credential helper for docker to access Amazon ECR
+echo '\e[34mExecuting\e[0m "\e[32mgo \e[37mgithub.com/awslabs/amazon-ecr-credential-helper/ecr-login/cli/docker-credential-ecr-login@latest\e[0m"'
+go install github.com/awslabs/amazon-ecr-credential-helper/ecr-login/cli/docker-credential-ecr-login@latest
 # Tool to fetch gitignore files from GitHub templates `gig`
 echo '\e[34mExecuting\e[0m "\e[32mgo \e[37minstall github.com/toshi0607/gig@latest\e[0m"'
 go install github.com/toshi0607/gig@latest
