38344ce72ce7aa51eb0e2cd612036573998457ee
AnimiVulpis <animi.vulpis@gmail.com>
2021-12-07 11:26:23 +0100

    Update scripts

--- scripts/repository-updates.zsh
+++ scripts/repository-updates.zsh
@@ -12,15 +12,15 @@ echo -ne '\e[?25l'
 # Fetch updates and print errors
 exa -D $1 \
     | xargs -I@ zsh -c "
-    echo -ne '\e[2KFetching @\r';
+    echo -ne '\e[2K\e[34mFetching\e[0m @\r';
     git -C $1/@ fetch -q origin || echo -e 'Fetching @ \e[0;31mfailed\e[0m (See above for error)'"
 
 # Pretty-print tracking information
 exa -D $1 \
     | xargs -I@ zsh -c "
     echo -n '@\t';
-    git -c color.status=always -C $1/@ status --untracked-files=no --short --branch" 2>&1 \
-    | column --table --separator '	'
+    git -c color.status=always -C $1/@ status --untracked-files=no --short --branch | head -n1" 2>&1 \
+    | column -t -s '	'
 
 # Show cursor again
 echo -ne '\e[?25h'
