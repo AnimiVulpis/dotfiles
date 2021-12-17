c9633668b2ff14bd55ea002c1a8ba92c7d12912b
AnimiVulpis <animi.vulpis@gmail.com>
2021-12-17 11:41:37 +0100

    More changes to i3 related configuration

--- scripts/update-more.sh
+++ scripts/update-more.sh
@@ -7,7 +7,7 @@ rustup update
 # List all installed rust crates
 # cargo install --list
 
-echo '\e[34mExecuting\e[0m "\e[32mcargo \e[37minstall bat broot click eva exa fd-find find_unicode genact git-delta hyperfine ncspot swaysome sworkstyle tidy-viewer \e[0m"'
+echo '\e[34mExecuting\e[0m "\e[32mcargo \e[37minstall bat broot click eva exa fd-find find_unicode genact git-delta hyperfine ncspot swaysome sworkstyle tidy-viewer workstyle \e[0m"'
 cargo install \
     bat \
     broot \
@@ -22,7 +22,8 @@ cargo install \
     ncspot \
     swaysome \
     sworkstyle \
-    tidy-viewer
+    tidy-viewer \
+    workstyle
 cargo install --version 0.1.0-alpha.5 gobang
 
 # Go related
