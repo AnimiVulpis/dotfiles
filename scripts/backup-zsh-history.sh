3908c82ddd53771bc1fe9b723268823c685c4845
AnimiVulpis <animi.vulpis@gmail.com>
2022-03-24 00:39:32 +0100

    Improve zsh history back-up script

--- scripts/backup-zsh-history.sh
+++ scripts/backup-zsh-history.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 history_location="$HOME/.local/share/zsh/history-file"
-
 lines_in_history=$(wc -l $history_location | cut -d ' ' -f 1)
 
-echo "Currently $lines_in_history lines in $history_location"
+backup_folder="$HOME/projects/zsh-history-backup/"
+lines_in_latest_backup=$(ls -r $backup_folder | head -n 1 | cut -d 'L' -f 2)
 
-cp -v $history_location ~/projects/zsh-history-backup/$(date -Im)-$lines_in_history-histfile
+echo "Currently $lines_in_history lines in $history_location"
+echo "Lastest backup has $lines_in_latest_backup lines"
+
+cp -v $history_location $backup_folder$(date -Im)_histfile_L$lines_in_history
