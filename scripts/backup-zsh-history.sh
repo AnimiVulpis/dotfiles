6ef67d485702cd6e2550ed4f607ee29c027f2f2c
AnimiVulpis <animi.vulpis@gmail.com>
2022-04-01 08:33:15 +0200

    Fix typo

--- scripts/backup-zsh-history.sh
+++ scripts/backup-zsh-history.sh
@@ -7,6 +7,6 @@ backup_folder="$HOME/projects/zsh-history-backup/"
 lines_in_latest_backup=$(ls -r $backup_folder | head -n 1 | cut -d 'L' -f 2)
 
 echo "Currently $lines_in_history lines in $history_location"
-echo "Lastest backup has $lines_in_latest_backup lines"
+echo "Latest backup has $lines_in_latest_backup lines"
 
 cp -v $history_location $backup_folder$(date -Im)_histfile_L$lines_in_history
