424c6c7142a2464578751753d31d2a58cd317315
AnimiVulpis <animi.vulpis@gmail.com>
2023-01-26 09:04:23 +0100

    Improve backup and add zsh history

--- scripts/rclone-backup.sh
+++ scripts/rclone-backup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo 'cp -fv ~/.local/share/buku/* ~/rclone/'
-cp -fv ~/.local/share/buku/* ~/rclone/
+echo 'cp -pfv ~/.local/share/buku/* ~/rclone/'
+cp -pfv ~/.local/share/buku/* ~/rclone/
 
 echo 'rclone --progress --human-readable -v bisync ~/todotxt/ gdrive:todotxt/'
-rclone --progress --human-readable -v bisync ~/todotxt/ gdrive:todotxt/
+rclone --progress --human-readable -v bisync ~/todotxt/ gdrive:todotxt/ || exit 1
 
 echo 'rclone --progress --human-readable -v bisync ~/rclone/ gdrive:rclone/'
 rclone --progress --human-readable -v bisync ~/rclone/ gdrive:rclone/
