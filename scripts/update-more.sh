12e1e2deaa074442951a44e7463923239004e510
AnimiVulpis <animi.vulpis@gmail.com>
2023-03-28 10:40:25 +0200

    Add/Update installed "things"

--- scripts/update-more.sh
+++ scripts/update-more.sh
@@ -33,7 +33,6 @@ echo '\e[34mExecuting\e[0m "\e[32mcargo \e[37minstall
     jless \
     jwt-cli \
     ncspot \
-    sheldon \
     tidy-viewer \
     tokei \e[0m"'
 
@@ -56,37 +55,89 @@ cargo install \
     jless \
     jwt-cli \
     ncspot \
-    sheldon \
     tidy-viewer \
     tokei
-cargo install --version 0.1.0-alpha.5 gobang
 
+# ##############################
 # Go related
 
 # List all installed go packages
-# go list ...
+# go list ... or ls -lA ~/go/bin
+
+# Terminal confetti tool
+echo '\e[34mExecuting\e[0m "\e[32mgo \e[37minstall github.com/maaslalani/confetty@latest\e[0m"'
+go install github.com/maaslalani/confetty@latest
+
+# Terminal countdown tool
+echo '\e[34mExecuting\e[0m "\e[32mgo \e[37minstall github.com/antonmedv/countdown@latest\e[0m"'
+go install github.com/antonmedv/countdown@latest
 
 # Credential helper for docker to access Amazon ECR
 echo '\e[34mExecuting\e[0m "\e[32mgo \e[37minstall github.com/awslabs/amazon-ecr-credential-helper/ecr-login/cli/docker-credential-ecr-login@latest\e[0m"'
 go install github.com/awslabs/amazon-ecr-credential-helper/ecr-login/cli/docker-credential-ecr-login@latest
-# Tool to manage hosts file idempotent `hostess`
-echo '\e[34mExecuting\e[0m "\e[32mgo \e[37minstall github.com/cbednarski/hostess@latest\e[0m"'
-go install github.com/cbednarski/hostess@latest
+
 # Tool for disk space analysis
 echo '\e[34mExecuting\e[0m "\e[32mgo \e[37minstall github.com/dundee/gdu/v5/cmd/gdu@latest\e[0m"'
 go install github.com/dundee/gdu/v5/cmd/gdu@latest
-# Tool for fast code search `houndd`
-echo '\e[34mExecuting\e[0m "\e[32mgo \e[37minstall github.com/hound-search/hound/cmds/...@latest\e[0m"'
-go install github.com/hound-search/hound/cmds/...@latest
+
 # Tool to fetch gitignore files from GitHub templates `gig`
 echo '\e[34mExecuting\e[0m "\e[32mgo \e[37minstall github.com/toshi0607/gig@latest\e[0m"'
 go install github.com/toshi0607/gig@latest
+
 # Tool to check the size of git repositories `git-sizer`
 echo '\e[34mExecuting\e[0m "\e[32mgo \e[37minstall github.com/github/git-sizer@latest\e[0m"'
 go install github.com/github/git-sizer@latest
+
+# Go language server
+# go install golang.org/x/tools/gopls@latest
+
+# Tool to manage hosts file idempotent `hostess`
+echo '\e[34mExecuting\e[0m "\e[32mgo \e[37minstall github.com/cbednarski/hostess@latest\e[0m"'
+go install github.com/cbednarski/hostess@latest
+
+# Tool for fast code search `houndd`
+echo '\e[34mExecuting\e[0m "\e[32mgo \e[37minstall github.com/hound-search/hound/cmds/...@latest\e[0m"'
+go install github.com/hound-search/hound/cmds/...@latest
+
+# technology detector tool
+echo '\e[34mExecuting\e[0m "\e[32mgo \e[37minstall github.com/projectdiscovery/httpx/cmd/httpx@latest\e[0m"'
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+
+# terminal file manager
+echo '\e[34mExecuting\e[0m "\e[32menv CGO_ENABLED=0 go \e[37minstall -ldflags="-s -w" github.com/gokcehan/lf@latest\e[0m"'
+env CGO_ENABLED=0 go install -ldflags="-s -w" github.com/gokcehan/lf@latest
+
+# minio
+# echo '\e[34mExecuting\e[0m "\e[32mgo \e[37minstall github.com/minio/mc@latest\e[0m"'
+# go install github.com/minio/mc@latest
+
+#minica
+echo '\e[34mExecuting\e[0m "\e[32mgo \e[37minstall github.com/jsha/minica@latest\e[0m"'
+go install github.com/jsha/minica@latest
+
+# rclone update
+echo "To update rclone"
+echo "sudo -v ; curl https://rclone.org/install.sh | sudo bash"
+
+# Shell format
+echo '\e[34mExecuting\e[0m "\e[32mgo \e[37minstall mvdan.cc/sh/v3/cmd/shfmt@latest\e[0m"'
+go install mvdan.cc/sh/v3/cmd/shfmt@latest
+
+# staticcheck from https://github.com/dominikh/go-tools
+
+# Subdomain enumeration
+echo '\e[34mExecuting\e[0m "\e[32mgo \e[37minstall -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest\e[0m"'
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+
 # Tool to visualize most used cli commands `tsukae`
 echo '\e[34mExecuting\e[0m "\e[32mgo \e[37minstall github.com/irevenko/tsukae@latest\e[0m"'
 go install github.com/irevenko/tsukae@latest
+
+# Modern watch alternative
+echo '\e[34mExecuting\e[0m "\e[32mgo \e[37minstall github.com/sachaos/viddy@latest\e[0m"'
+go install github.com/sachaos/viddy@latest
+
+# ##############################
 
 echo '\e[34mExecuting\e[0m "\e[32m~/dotfiles/scripts/repository-updates.zsh \e[35m~/external-software/\e[0m"'
 ~/dotfiles/scripts/repository-updates.zsh ~/external-repos/
