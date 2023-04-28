45d553d464593a472c4d75ef0c23f72d81a18d6a
AnimiVulpis <animi.vulpis@gmail.com>
2023-04-28 11:56:40 +0200

    Improve the update-more script

--- scripts/repository-updates.zsh
+++ scripts/repository-updates.zsh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 
 # Check if first argument is a valid directory
 if [ ! -d "$1" ]; then
@@ -8,19 +8,25 @@ fi
 
 # Hide cursor
 echo -ne '\e[?25l'
+# Show cursor when exiting with CTRL+C
+trap "echo -e '\e[?25h'" SIGINT
 
-# Fetch updates and print errors
+# Fetch updates and print potential errors
 exa -D $1 \
-    | xargs -I@ zsh -c "
-    echo -ne '\e[2K\e[34mFetching\e[0m @\r';
-    git -C $1/@ fetch -q origin || echo -e 'Fetching @ \e[0;31mfailed\e[0m (See above for error)'"
+    | xargs -I@ bash -c "\
+        echo -ne \"\e[34mFetching\e[0m @\e[30G\";\
+        error_message=\$(git -C $1/@ fetch -q origin 2>&1)\
+            && echo -ne '\e[32mOK\e[0m\e[35G';\
+                git -c color.status=always -C $1/@ status --untracked-files=no --short --branch | head -n1 \
+            || echo -e \"\e[31mFAILED\e[0m\n\$error_message\"\
+    "
 
-# Pretty-print tracking information
-exa -D $1 \
-    | xargs -I@ zsh -c "
-    echo -n '@\t';
-    git -c color.status=always -C $1/@ status --untracked-files=no --short --branch | head -n1" 2>&1 \
-    | column -t -s '	'
+# Pretty-print tracking information (summary)
+# exa -D $1 \
+#     | xargs -I@ zsh -c "
+#     echo -n '@\t';
+#     git -c color.status=always -C $1/@ status --untracked-files=no --short --branch | head -n1" 2>&1 \
+#     | column -t -s '	'
 
 # Show cursor again
 echo -ne '\e[?25h'
