9c55cf0d07246815833ac95a6a61230ee9900b4e
AnimiVulpis <animi.vulpis@gmail.com>
2023-09-27 11:39:45 +0200

    Remove todotxt as I stopped using it

--- scripts/rclone-backup.sh
+++ scripts/rclone-backup.sh
@@ -3,8 +3,5 @@
 echo 'cp -pfv ~/.local/share/buku/* ~/rclone/'
 cp -pfv ~/.local/share/buku/* ~/rclone/
 
-echo 'rclone --progress --human-readable -v bisync ~/todotxt/ gdrive:todotxt/'
-rclone --progress --human-readable -v bisync ~/todotxt/ gdrive:todotxt/ || exit 1
-
 echo 'rclone --progress --human-readable -v bisync ~/rclone/ gdrive:rclone/'
 rclone --progress --human-readable -v bisync ~/rclone/ gdrive:rclone/
