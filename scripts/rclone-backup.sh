97a8e580a1a70edd9381460488bb4eda7e6482c7
AnimiVulpis <animi.vulpis@gmail.com>
2023-11-03 10:29:37 +0100

    Change scripts and add to path

--- scripts/rclone-backup.sh
+++ scripts/rclone-backup.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-echo 'cp -pfv ~/.local/share/buku/* ~/rclone/'
-cp -pfv ~/.local/share/buku/* ~/rclone/
-
 echo 'rclone --progress --human-readable -v bisync ~/rclone/ gdrive:rclone/'
 rclone --progress --human-readable -v bisync ~/rclone/ gdrive:rclone/
