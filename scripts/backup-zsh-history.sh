9d3209feb98fa6069f7cc160406608094aca45f9
AnimiVulpis <animi.vulpis@gmail.com>
2023-11-15 08:57:45 +0100

    Fix shellcheck warning

--- scripts/backup-zsh-history.sh
+++ scripts/backup-zsh-history.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 history_location="$HOME/.local/share/zsh/history-file"
-lines_in_history=$(wc -l $history_location | cut -w -f 2)
+lines_in_history=$(wc -l "$history_location" | cut -w -f 2)
 
 backup_folder="$HOME/projects/zsh-history-backup/"
-lines_in_latest_backup=$(ls -r $backup_folder | head -n 1 | cut -d 'L' -f 2)
+lines_in_latest_backup=$(exa "$backup_folder" -s name -r | head -n 1 | cut -d 'L' -f 2)
 
 echo "Currently $lines_in_history lines in $history_location"
 echo "Latest backup has $lines_in_latest_backup lines"
 
-cp -v $history_location $backup_folder$(date -Iminutes)_histfile_L$lines_in_history
+cp -v "$history_location" "$backup_folder""$(date -Iminutes)"_histfile_L"$lines_in_history"
 
-cp -pfv $history_location ~/rclone/
+cp -pfv "$history_location" ~/rclone/
