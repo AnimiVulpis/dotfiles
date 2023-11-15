87bec821b665773bd94149e9b0db7e018e95d781
AnimiVulpis <animi.vulpis@gmail.com>
2023-11-15 09:56:55 +0100

    Refactor and improve backup-zsh-history script

--- scripts/backup-zsh-history.sh
+++ scripts/backup-zsh-history.sh
@@ -1,14 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e # Exit if a command exits with a non-zero status
+set -u # Treat unset variables as an error
+set -o pipefail # Treat any non-zero status in a pipeline like a total pipeline failure
+shopt -s inherit_errexit # Command substitutions inherit set -e from the parent script
 
 history_location="$HOME/.local/share/zsh/history-file"
 lines_in_history=$(wc -l "$history_location" | cut -w -f 2)
 
 backup_folder="$HOME/projects/zsh-history-backup/"
-lines_in_latest_backup=$(exa "$backup_folder" -s name -r | head -n 1 | cut -d 'L' -f 2)
 
-echo "Currently $lines_in_history lines in $history_location"
-echo "Latest backup has $lines_in_latest_backup lines"
+latest_backup=$(exa "$backup_folder" -rs newest | head -n 1)
+lines_in_latest_backup=$(echo "$latest_backup" | rg -e 'L(\d+)' -or '$1')
 
-cp -v "$history_location" "$backup_folder""$(date -Iminutes)"_histfile_L"$lines_in_history"
+lines_in_largest_backup=$(exa "$backup_folder" | rg -e 'L(\d+)' -or '$1' | sort -nr | head -n 1)
+largest_backup=$(exa "$backup_folder" | rg -e "L$lines_in_largest_backup")
+
+echo "$lines_in_history lines currently in $history_location"
+echo "$lines_in_latest_backup lines in latest backup ($latest_backup)"
+echo "$lines_in_largest_backup lines in largest backup ($largest_backup)"
+
+cp -v "$history_location" "$backup_folder"L"$lines_in_history"_histfile_"$(date -Iminutes)"
 
 cp -pfv "$history_location" ~/rclone/
