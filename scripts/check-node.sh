c44a5680af4524373e78d6782b1ce4074f389777
AnimiVulpis <animi.vulpis@gmail.com>
2023-11-29 11:45:26 +0100

    Add hint to check for node(nvm) updates

---
+++ scripts/check-node.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e # Exit if a command exits with a non-zero status
+set -u # Treat unset variables as an error
+set -o pipefail # Treat any non-zero status in a pipeline like a total pipeline failure
+shopt -s inherit_errexit # Command substitutions inherit set -e from the parent script
+
+echo Execute the following line to check for updates
+echo "nvm ls-remote --lts | rg --multiline -e '(:?.*\n){0,3}.*Latest ' && nvm ls-remote | tail -n 3"
