fd86c1e2e4bc287ffac5b1fe488b828492f68348
AnimiVulpis <animi.vulpis@gmail.com>
2023-11-30 14:20:18 +0100

    Switch to new prelude for scripts
    !update-more might not work correctly anymore!

--- scripts/do-nothing-template.sh
+++ scripts/do-nothing-template.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e # Exit if a command exits with a non-zero status
+set -u # Treat unset variables as an error
+set -o pipefail # Treat any non-zero status in a pipeline like a total pipeline failure
+shopt -s inherit_errexit # Command substitutions inherit set -e from the parent script
 
 read -n 1 -s -r -p "Press any key to continue..."; echo
 # -n ## defines the required character count to stop reading
--- scripts/wake-up-nas.sh
+++ scripts/wake-up-nas.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e # Exit if a command exits with a non-zero status
+set -u # Treat unset variables as an error
+set -o pipefail # Treat any non-zero status in a pipeline like a total pipeline failure
+shopt -s inherit_errexit # Command substitutions inherit set -e from the parent script
 
 echo "wakeonlan -i <ip> <mac-address>"
 echo "Example:"
