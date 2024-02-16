bb9cdd63ec1a7254aaf92801d062028628cc5eb2
AnimiVulpis <animi.vulpis@gmail.com>
2024-02-16 16:45:31 +0100

    Change to `printf` to satisfy shellcheck(SC2028)

--- scripts/check-node.sh
+++ scripts/check-node.sh
@@ -5,4 +5,7 @@ set -o pipefail # Treat any non-zero status in a pipeline like a total pipeline 
 shopt -s inherit_errexit # Command substitutions inherit set -e from the parent script
 
 echo Execute the following line to check for updates
-echo "nvm ls-remote --lts | rg --multiline --colors=match:none -e '(:?.*\n){0,3}.*Latest ' && nvm ls-remote | tail -n 3"
+# The command is:
+# nvm ls-remote --lts | rg --multiline --colors=match:none -e '(:?.*\n){0,3}.*Latest ' && nvm ls-remote | tail -n 3
+# The double escaped newline is necessary to make printf print it out correctly
+printf "nvm ls-remote --lts | rg --multiline --colors=match:none -e '(:?.*\\\n){0,3}.*Latest ' && nvm ls-remote | tail -n 3\n"
