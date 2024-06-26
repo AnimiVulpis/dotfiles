be8aa8a89d63bffed264843fb6a611c8a94e0a20
AnimiVulpis <animi.vulpis@gmail.com>
2024-06-26 10:13:01 +0200

    Improve backup-zsh script by adding -i option
    to interactively (a)bort, (s)kip or (c)ontinue the script

--- scripts/backup-zsh-history.sh
+++ scripts/backup-zsh-history.sh
@@ -1,24 +1,71 @@
 #!/usr/bin/env bash
-set -e # Exit if a command exits with a non-zero status
-set -u # Treat unset variables as an error
-set -o pipefail # Treat any non-zero status in a pipeline like a total pipeline failure
+set -e                   # Exit if a command exits with a non-zero status
+set -u                   # Treat unset variables as an error
+set -o pipefail          # Treat any non-zero status in a pipeline like a total pipeline failure
 shopt -s inherit_errexit # Command substitutions inherit set -e from the parent script
 
-history_location="$HOME/.local/share/zsh/history-file"
-lines_in_history=$(wc -l "$history_location" | cut -w -f 2)
+interactive=false
 
-backup_folder="$HOME/projects/zsh-history-backup/"
+while getopts ":i" opt; do
+    case ${opt} in
+    i)
+        interactive=true
+        ;;
+    ?)
+        echo "Invalid option: -${OPTARG}"
+        exit 1
+        ;;
+    esac
+done
 
-latest_backup=$(eza "$backup_folder" -rs newest | head -n 1)
-lines_in_latest_backup=$(echo "$latest_backup" | rg -e 'L(\d+)' -or '$1')
+backup_zsh_history() {
+    local history_location
+    local lines_in_history
+    local backup_folder
+    local latest_backup
+    local lines_in_latest_backup
+    local lines_in_largest_backup
+    local largest_backup
+    history_location="$HOME/.local/share/zsh/history-file"
+    lines_in_history=$(wc -l "$history_location" | cut -w -f 2)
 
-lines_in_largest_backup=$(eza "$backup_folder" | rg -e 'L(\d+)' -or '$1' | sort -nr | head -n 1)
-largest_backup=$(eza "$backup_folder" | rg -e "L$lines_in_largest_backup")
+    backup_folder="$HOME/projects/zsh-history-backup/"
 
-echo "$lines_in_history lines currently in $history_location"
-echo "$lines_in_latest_backup lines in  latest backup ($latest_backup)"
-echo "$lines_in_largest_backup lines in largest backup ($largest_backup)"
+    latest_backup=$(eza "$backup_folder" -rs newest | head -n 1)
+    lines_in_latest_backup=$(echo "$latest_backup" | rg -e 'L(\d+)' -or '$1')
 
-cp -v "$history_location" "$backup_folder"L"$lines_in_history"_histfile_"$(date -Iminutes)"
+    lines_in_largest_backup=$(eza "$backup_folder" | rg -e 'L(\d+)' -or '$1' | sort -nr | head -n 1)
+    largest_backup=$(eza "$backup_folder" | rg -e "L$lines_in_largest_backup")
 
-cp -pfv "$history_location" ~/rclone/
+    echo "$lines_in_history lines currently in $history_location"
+    echo "$lines_in_latest_backup lines in  latest backup ($latest_backup)"
+    echo "$lines_in_largest_backup lines in largest backup ($largest_backup)"
+
+    cp -v "$history_location" "$backup_folder"L"$lines_in_history"_histfile_"$(date -Iminutes)"
+
+    cp -pfv "$history_location" ~/rclone/
+}
+
+if $interactive; then
+    read -n 1 -s -r -p "abort, skip, or continue backup-zsh-history? [a/s/c]: " choice
+    case $choice in
+    a)
+        echo "Aborting..."
+        exit 1
+        ;;
+    s)
+        echo "Skipping..."
+        exit 0
+        ;;
+    c)
+        echo
+        backup_zsh_history
+        ;;
+    *)
+        echo "Invalid choice. Aborting..."
+        exit 1
+        ;;
+    esac
+else
+    backup_zsh_history
+fi
