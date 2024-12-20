e80685dcb74528354e3e184ba67532cfffd5983d
AnimiVulpis <animi.vulpis@gmail.com>
2024-12-20 12:30:35 +0100

    Fix typo

--- scripts/wake-up-nas.sh
+++ scripts/wake-up-nas.sh
@@ -9,5 +9,5 @@ echo "Example:"
 echo "wakeonlan -i 192.168.178.255 00:11:22:33:44:55"
 echo
 echo "After successfull wake-up run the following to mount:"
-echo "sudo mount -t nfs <name>:<remote-path/to-mount> <local-path/to-mount-to/"
+echo "sudo mount -t nfs <name>:<remote-path/to-mount> <local-path/to-mount-to/>"
 echo "sudo mount -t nfs nas:/volume1/nas-data /path/to/mount/nas/"
