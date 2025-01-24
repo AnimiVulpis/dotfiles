d9278cbf8c84bc575b4574f7b958edb927e4df26
AnimiVulpis <animi.vulpis@gmail.com>
2025-01-24 12:20:09 +0100

    Use same command to have same order of folders in scripts

--- scripts/check-subfolders-repo-status.zsh
+++ scripts/check-subfolders-repo-status.zsh
@@ -5,7 +5,7 @@ set -o pipefail # Treat any non-zero status in a pipeline like a total pipeline 
 
 # The basis for this script is this command I used:
 # for folder in ./*/; do echo -n "${(r:30:::::)folder}"; git -C $folder status --untracked-files=no --short --branch; done
-for subfolder in ./*/; do
+for subfolder in $(eza -D ./); do
     echo -n "${(r:32:::::)subfolder}"
     # If it's not a git repository, git will return an error that needs to be suppressed
     git_status=$(git -c color.status=always -C $subfolder status --untracked-files=no --short --branch 2>/dev/null) &&
