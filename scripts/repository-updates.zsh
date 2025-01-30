2ecc477c42be8029dfdf4179edc66fc28bd873c0
AnimiVulpis <animi.vulpis@gmail.com>
2025-01-30 11:17:37 +0100

    Improve script with learnings from the improvements before
    Based on the learnings from updating `check-subfolders-repo-status.zsh`.
    
    Removed the cursor hiding logic, because I don't need that

--- scripts/repository-updates.zsh
+++ scripts/repository-updates.zsh
@@ -1,8 +1,8 @@
-#!/usr/bin/env bash
-set -e                   # Exit if a command exits with a non-zero status
-set -u                   # Treat unset variables as an error
-set -o pipefail          # Treat any non-zero status in a pipeline like a total pipeline failure
-shopt -s inherit_errexit # Command substitutions inherit set -e from the parent script
+#!/usr/bin/env zsh
+set -e              # Exit if a command exits with a non-zero status
+set -u              # Treat unset variables as an error
+set -o pipefail     # Treat any non-zero status in a pipeline like a total pipeline failure
+set -o localoptions # Ensure options are local to the function or script
 
 # Check if first argument is a valid directory
 if [ ! -d "$1" ]; then
@@ -10,27 +10,28 @@ if [ ! -d "$1" ]; then
     exit 0
 fi
 
-# Hide cursor
-echo -ne '\e[?25l'
-# Show cursor when exiting with CTRL+C
-trap "echo -e '\e[?25h'" SIGINT
+update_subfolder_repos() {
+    local git_fetch_error git_status git_diff
+    local subfolder_list=($(eza -D ./))
+    local max_length=40 # Plus one char on each side makes 42^^
+    for subfolder in ${subfolder_list[@]}; do
+        if (( ${#subfolder} > max_length )); then
+            max_length=${#subfolder}
+        fi
+    done
+    for subfolder in ${subfolder_list[@]}; do
+        # Use the Box Drawing Light Left (U+2574) character for padding
+        echo -n "${(r:max_length::╴:: :)subfolder}"
+        echo -n " \e[34mFetching\e[0m"
+        # If it's not a git repository, git will return an error that needs to be suppressed
+        git_fetch_error=$(git -C $subfolder fetch --quiet origin 2>&1) &&
+            echo -n " \e[32mOK\e[0m" &&
+            git_status=$(git -c color.status=always -C $subfolder status --untracked-files=no --short --branch 2>/dev/null) &&
+            echo " $git_status" | head -n1 | tr -d '\n' &&
+            git_diff=$(git -C $subfolder diff --shortstat | tail -n1) &&
+            echo -e "\e[33m$git_diff\e[m" ||
+            echo -e " \e[31mFAILED\e[0m \e[36m${git_fetch_error}\e[0m"
+    done
+}
 
-# Fetch updates and print potential errors
-eza -D $1 |
-    xargs -S512 -I@ bash -c "\
-        echo -ne \"\e[34mFetching\e[0m @\e[34G\";\
-        error_message=\$(git -C $1/@ fetch -q origin 2>&1)\
-            && echo -ne '\e[32mOK\e[0m\e[37G';\
-                git -c color.status=always -C $1/@ status --untracked-files=no --short --branch | head -n1 \
-            || echo -e \"\e[31mFAILED\e[0m\n\$error_message\"\
-    "
-
-# Pretty-print tracking information (summary)
-# eza -D $1 \
-#     | xargs -I@ zsh -c "
-#     echo -n '@\t';
-#     git -c color.status=always -C $1/@ status --untracked-files=no --short --branch | head -n1" 2>&1 \
-#     | column -t -s '	'
-
-# Show cursor again
-echo -ne '\e[?25h'
+update_subfolder_repos "$1"
