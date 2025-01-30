56f7d22340e733d968f9a334ad55696d157830b4
AnimiVulpis <animi.vulpis@gmail.com>
2025-01-30 11:28:18 +0100

    Add a comment for the change-some-colors script

--- scripts/change-some-colors.sh
+++ scripts/change-some-colors.sh
@@ -6,3 +6,6 @@ shopt -s inherit_errexit # Command substitutions inherit set -e from the parent 
 
 sd ":\*LS_COLORS=48;5;89;38;5;197;1;3;4;7:" ":*LS_COLORS=48;5;89;38;5;9;1;3;4;7:" "$1"
 sd ":no=0:" ":no=38;5;248:" "$1"
+
+# I used this to alter the default colors supplied by https://github.com/trapd00r/LS_COLORS
+# by running `change-some-colors.sh lscolors.sh #lscolors` inside that repo
